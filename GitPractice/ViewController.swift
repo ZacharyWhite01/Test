d6b2659198aaf9ec7261b11f801e2e913e8fbd52
allen welch <allenwelch@maed-5.local>
2017-03-08 10:40:50 -0700

    commit

--- GitPractice/ViewController.swift
+++ GitPractice/ViewController.swift
@@ -9,10 +9,13 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    var meow = "meow"
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        for i in 0...16{
+            print(meow)
+        }
     }
 
     override func didReceiveMemoryWarning() {
